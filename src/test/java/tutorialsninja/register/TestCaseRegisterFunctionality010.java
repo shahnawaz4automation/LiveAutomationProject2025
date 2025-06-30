110062ad70bf7962c164a75fe8db2c33c406c3af
Mohammed Shahnawaz <shah4corp@gmail.com>
2025-06-30 21:58:08 +0530

    TestCaseRegisterFunctionality010 partially automated

--- src/test/java/tutorialsninja/register/TestCaseRegisterFunctionality010.java
+++ src/test/java/tutorialsninja/register/TestCaseRegisterFunctionality010.java
@@ -1,10 +1,14 @@
 package tutorialsninja.register;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class TestCaseRegisterFunctionality010 {
 	@Test
 	public void verifyRegisteringUsingInvalidEmail() {
-
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().op
 	}
 }
