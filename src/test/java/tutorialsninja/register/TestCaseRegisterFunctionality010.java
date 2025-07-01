e0258c661a2aacd9856b98d3b953955c295a86f6
Mohammed Shahnawaz <shah4corp@gmail.com>
2025-07-01 11:57:24 +0530

    TestCaseRegisterFunctionality010 test case automated

--- src/test/java/tutorialsninja/register/TestCaseRegisterFunctionality010.java
+++ src/test/java/tutorialsninja/register/TestCaseRegisterFunctionality010.java
@@ -1,14 +1,113 @@
 package tutorialsninja.register;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+
+import javax.imageio.ImageIO;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.io.FileHandler;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import ru.yandex.qatools.ashot.comparison.ImageDiff;
+import ru.yandex.qatools.ashot.comparison.ImageDiffer;
 
 public class TestCaseRegisterFunctionality010 {
 	@Test
-	public void verifyRegisteringUsingInvalidEmail() {
-		WebDriver driver = new ChromeDriver();
+	public void verifyRegisteringUsingInvalidEmailPlusOffline() throws Exception {
+
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless=new"); // Use --headless=new instead of --headless in latest Chrome
+		options.addArguments("--window-size=1920,1080");
+		WebDriver driver = new ChromeDriver(options);
+		
 		driver.manage().window().maximize();
-		driver.manage().op
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+		driver.get("https://tutorialsninja.com/demo");
+		
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("arguments[0].click();", driver.findElement(By.xpath("//span[text()='My Account']")));
+		
+		//driver.findElement(By.xpath("//span[text()='My Account']")).click();
+		driver.findElement(By.linkText("Register")).click();
+
+		driver.findElement(By.id("input-firstname")).sendKeys("Arun");
+		driver.findElement(By.id("input-lastname")).sendKeys("Motoori");
+		driver.findElement(By.id("input-email")).sendKeys("amotoori");
+		driver.findElement(By.id("input-telephone")).sendKeys("1234567890");
+		driver.findElement(By.id("input-password")).sendKeys("12345");
+		driver.findElement(By.id("input-confirm")).sendKeys("12345");
+		driver.findElement(By.xpath("//input[@name='newsletter'][@value='1']")).click();
+		driver.findElement(By.name("agree")).click();
+		driver.findElement(By.xpath("//input[@value='Continue']")).click();
+
+		Thread.sleep(3000);
+
+		File srcScreenshot1 = driver.findElement(By.xpath("//form[@class='form-horizontal']")).getScreenshotAs(OutputType.FILE);
+		FileHandler.copy(srcScreenshot1, new File(System.getProperty("user.dir") + "\\Screenshots\\sc1Actual.png"));
+
+		Thread.sleep(3000);
+
+		Assert.assertFalse(compareTwoScreenshots(System.getProperty("user.dir") + "\\Screenshots\\sc1Actual.png",System.getProperty("user.dir") + "\\Screenshots\\sc1Expected.png"));
+
+		driver.findElement(By.id("input-email")).clear();
+		driver.findElement(By.id("input-email")).sendKeys("amotoori@");
+		driver.findElement(By.xpath("//input[@value='Continue']")).click();
+
+		Thread.sleep(2000);
+
+		File srcScreenshot2 = driver.findElement(By.xpath("//form[@class='form-horizontal']")).getScreenshotAs(OutputType.FILE);
+		FileHandler.copy(srcScreenshot2, new File(System.getProperty("user.dir") + "\\Screenshots\\sc2Actual.png"));
+
+		Thread.sleep(2000);
+
+		Assert.assertFalse(compareTwoScreenshots(System.getProperty("user.dir") + "\\Screenshots\\sc2Actual.png",
+				System.getProperty("user.dir") + "\\Screenshots\\sc2Expected.png"));
+
+		driver.findElement(By.id("input-email")).clear();
+		driver.findElement(By.id("input-email")).sendKeys("amotoori@gmail");
+		driver.findElement(By.xpath("//input[@value='Continue']")).click();
+
+		Thread.sleep(2000);
+
+		String expectedWarningMessage = "E-Mail Address does not appear to be valid!";
+		Thread.sleep(2000);
+		Assert.assertEquals(driver.findElement(By.xpath("//input[@id='input-email']/following-sibling::div")).getText(),
+				expectedWarningMessage);
+
+		driver.findElement(By.id("input-email")).clear();
+		driver.findElement(By.id("input-email")).sendKeys("amotoori@gmail.");
+		driver.findElement(By.xpath("//input[@value='Continue']")).click();
+
+		Thread.sleep(3000);
+
+		File srcScreenshot3 = driver.findElement(By.xpath("//form[@class='form-horizontal']")).getScreenshotAs(OutputType.FILE);
+		FileHandler.copy(srcScreenshot3, new File(System.getProperty("user.dir") + "\\Screenshots\\sc3Actual.png"));
+
+		Thread.sleep(3000);
+
+		Assert.assertFalse(compareTwoScreenshots(System.getProperty("user.dir") + "\\Screenshots\\sc3Actual.png",
+				System.getProperty("user.dir") + "\\Screenshots\\sc3Expected.png"));
+
+		driver.quit();
+
+	}
+
+	public boolean compareTwoScreenshots(String actualImagePath, String expectedImagePath) throws IOException {
+		BufferedImage actualImg = ImageIO.read(new File(actualImagePath));
+		BufferedImage expectedImg = ImageIO.read(new File(expectedImagePath));
+
+		ImageDiffer imgDiffer = new ImageDiffer();
+		ImageDiff imgDifference = imgDiffer.makeDiff(expectedImg, actualImg);
+
+		return imgDifference.hasDiff();
 	}
 }
