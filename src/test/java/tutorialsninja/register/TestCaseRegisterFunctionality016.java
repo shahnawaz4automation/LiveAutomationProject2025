d878714164a54e7cb5d09e96eb0aa159e6902709
Mohammed Shahnawaz <shah4corp@gmail.com>
2025-07-02 22:56:31 +0530

    TC16 commit

--- src/test/java/tutorialsninja/register/TestCaseRegisterFunctionality016.java
+++ src/test/java/tutorialsninja/register/TestCaseRegisterFunctionality016.java
@@ -36,20 +36,11 @@ public class TestCaseRegisterFunctionality016 {
 		String telephoneWarning = "Telephone must be between 3 and 32 characters!";
 		String passwordWarning = "Password must be between 4 and 20 characters!";
 
-		Assert.assertEquals(
-				driver.findElement(By.xpath("//input[@id='input-firstname']/following-sibling::div")).getText(),
-				firstNameWarning);
-		Assert.assertEquals(
-				driver.findElement(By.xpath("//input[@id='input-lastname']/following-sibling::div")).getText(),
-				lastNameWarning);
-		Assert.assertEquals(driver.findElement(By.xpath("//input[@id='input-email']/following-sibling::div")).getText(),
-				emailWarning);
-		Assert.assertEquals(
-				driver.findElement(By.xpath("//input[@id='input-telephone']/following-sibling::div")).getText(),
-				telephoneWarning);
-		Assert.assertEquals(
-				driver.findElement(By.xpath("//input[@id='input-password']/following-sibling::div")).getText(),
-				passwordWarning);
+		Assert.assertEquals(driver.findElement(By.xpath("//input[@id='input-firstname']/following-sibling::div")).getText(), firstNameWarning);
+		Assert.assertEquals(driver.findElement(By.xpath("//input[@id='input-lastname']/following-sibling::div")).getText(), lastNameWarning);
+		Assert.assertEquals(driver.findElement(By.xpath("//input[@id='input-email']/following-sibling::div")).getText(), emailWarning);
+		Assert.assertEquals(driver.findElement(By.xpath("//input[@id='input-telephone']/following-sibling::div")).getText(), telephoneWarning);
+		Assert.assertEquals(driver.findElement(By.xpath("//input[@id='input-password']/following-sibling::div")).getText(),passwordWarning);
 
 		driver.quit();
 	}
