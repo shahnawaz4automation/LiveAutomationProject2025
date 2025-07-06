839575031f6eb1ab596d0cc6c5966680ef74734d
Mohammed Shahnawaz <shah4corp@gmail.com>
2025-07-06 20:57:19 +0530

    TestCaseRegisterFunctionality020 automated

---
+++ src/test/java/tutorialsninja/register/TestCaseRegisterFunctionality020.java
@@ -0,0 +1,29 @@
+package tutorialsninja.register;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestCaseRegisterFunctionality020 {
+	@Test
+	public void verifyPrivacyPolicyFieldOnRegisterAccountPage() {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+		driver.get("https://tutorialsninja.com/demo/");
+		
+		driver.findElement(By.xpath("//span[text()='My Account']")).click();
+		driver.findElement(By.linkText("Register")).click();
+		
+		Assert.assertFalse(driver.findElement(By.name("agree")).isSelected());
+		
+		driver.quit();
+		
+	}
+
+}
