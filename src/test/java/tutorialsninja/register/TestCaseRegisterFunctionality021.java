90cf75a353734fcfbb6068ebb7088c7b37560ec7
Mohammed Shahnawaz <shah4corp@gmail.com>
2025-07-06 21:01:13 +0530

    TestCaseRegisterFunctionality021 automated

---
+++ src/test/java/tutorialsninja/register/TestCaseRegisterFunctionality021.java
@@ -0,0 +1,40 @@
+package tutorialsninja.register;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import utils.CommonUtils;
+
+public class TestCaseRegisterFunctionality021 {
+	@Test
+	public void verifyRegisteringAccountWithoutPrivacyPolicySelection() {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+		driver.get("https://tutorialsninja.com/demo/");
+		
+		driver.findElement(By.xpath("//span[text()='My Account']")).click();
+		driver.findElement(By.linkText("Register")).click();
+		
+		driver.findElement(By.id("input-firstname")).sendKeys("Arun");
+		driver.findElement(By.id("input-lastname")).sendKeys("Motoori");
+		driver.findElement(By.id("input-email")).sendKeys(CommonUtils.generateBrandNewEmail());
+		driver.findElement(By.id("input-telephone")).sendKeys("1234567890");
+		driver.findElement(By.id("input-password")).sendKeys("12345");
+		driver.findElement(By.id("input-confirm")).sendKeys("12345");
+		driver.findElement(By.xpath("//input[@value='Continue']")).click();
+		
+		String expectedWarningMessage = "Warning: You must agree to the Privacy Policy!";
+		Assert.assertEquals(driver.findElement(By.xpath("//div[@class='alert alert-danger alert-dismissible']")).getText(), expectedWarningMessage);
+		
+		driver.quit();
+		
+	}
+
+}
