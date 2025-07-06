0f564d66ddeecceef04e6f8c4b711cc72c3c6895
Mohammed Shahnawaz <shah4corp@gmail.com>
2025-07-06 21:02:57 +0530

    TestCaseRegisterFunctionality022 automated

--- src/test/java/tutorialsninja/register/TestCaseRegisterFunctionality022.java
+++ src/test/java/tutorialsninja/register/TestCaseRegisterFunctionality022.java
@@ -1,5 +1,30 @@
 package tutorialsninja.register;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class TestCaseRegisterFunctionality022 {
+	@Test
+	public void verifyVisibiltyTogglineOfPasswordsFieldsOnRegisterAccount() {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+		driver.get("https://tutorialsninja.com/demo/");
+		
+		driver.findElement(By.xpath("//span[text()='My Account']")).click();
+		driver.findElement(By.linkText("Register")).click();
+		
+		Assert.assertEquals(driver.findElement(By.id("input-password")).getAttribute("type"), "password");
+		Assert.assertEquals(driver.findElement(By.id("input-confirm")).getAttribute("type"), "password");
+		
+		driver.quit();
+		
+	}
 
 }
