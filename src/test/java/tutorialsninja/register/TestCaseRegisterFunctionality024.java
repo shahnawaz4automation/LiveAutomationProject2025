62d60a9557c248a763da6f7e770349968d3ddec4
Mohammed Shahnawaz <shah4corp@gmail.com>
2025-07-07 21:06:17 +0530

    TestCaseRegisterFunctionality024 automated

---
+++ src/test/java/tutorialsninja/register/TestCaseRegisterFunctionality024.java
@@ -0,0 +1,41 @@
+package tutorialsninja.register;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import utils.CommonUtils;
+
+public class TestCaseRegisterFunctionality024 {
+	@Test
+	public void verifyRegisteringAccountWithoutEnteringPasswordIntoPasswordConfirmField() {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+		driver.get("https://tutorialsninja.com/demo/");
+		
+		driver.findElement(By.xpath("//span[text()='My Account']")).click();
+		driver.findElement(By.linkText("Register")).click();
+		
+		driver.findElement(By.id("input-firstname")).sendKeys("Arun");
+		driver.findElement(By.id("input-lastname")).sendKeys("Motoori");
+		driver.findElement(By.id("input-email")).sendKeys(CommonUtils.generateBrandNewEmail());
+		driver.findElement(By.id("input-telephone")).sendKeys("1234567890");
+		driver.findElement(By.id("input-password")).sendKeys("12345");
+		driver.findElement(By.xpath("//input[@name='newsletter'][@value='1']")).click();
+		driver.findElement(By.name("agree")).click();
+		driver.findElement(By.xpath("//input[@value='Continue']")).click();
+		
+		String expectedWarning = "Password confirmation does not match password!";
+		Assert.assertEquals(driver.findElement(By.xpath("//input[@id='input-confirm']/following-sibling::div")).getText(), expectedWarning);
+		
+		driver.quit();
+		
+	}
+
+}
