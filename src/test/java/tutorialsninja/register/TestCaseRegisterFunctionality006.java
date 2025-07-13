4b7bf467899b79b5a88837bb895356c813dc67a7
Mohammed Shahnawaz <shah4corp@gmail.com>
2025-07-13 21:06:23 +0530

    TestCaseRegisterFunctionality006 updated

--- src/test/java/tutorialsninja/register/TestCaseRegisterFunctionality006.java
+++ src/test/java/tutorialsninja/register/TestCaseRegisterFunctionality006.java
@@ -1,13 +1,9 @@
 package tutorialsninja.register;
 
-import java.time.Duration;
-import java.util.Date;
+import java.util.Properties;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -19,12 +15,13 @@ import utils.CommonUtils;
 public class TestCaseRegisterFunctionality006 extends Base{
 	
 	WebDriver driver;
+	Properties prop;
 
 	@BeforeMethod
 	public void setUp() {
 		
 		driver = openBrowserAndApplication();
-
+		prop = CommonUtils.loadProperties();
 		driver.findElement(By.xpath("//span[text()='My Account']")).click();
 		driver.findElement(By.linkText("Register")).click();
 	}
@@ -38,12 +35,12 @@ public class TestCaseRegisterFunctionality006 extends Base{
 	@Test
 	public void verifyRegisteringAccountBySayingNoToNewsletter() {
 		
-		driver.findElement(By.id("input-firstname")).sendKeys("Arun");
-		driver.findElement(By.id("input-lastname")).sendKeys("Motoori");
+		driver.findElement(By.id("input-firstname")).sendKeys(prop.getProperty("firstName"));
+		driver.findElement(By.id("input-lastname")).sendKeys(prop.getProperty("lastName"));
 		driver.findElement(By.id("input-email")).sendKeys(CommonUtils.generateBrandNewEmail());
-		driver.findElement(By.id("input-telephone")).sendKeys("1234567890");
-		driver.findElement(By.id("input-password")).sendKeys("12345");
-		driver.findElement(By.id("input-confirm")).sendKeys("12345");
+		driver.findElement(By.id("input-telephone")).sendKeys(prop.getProperty("telephoneNumber"));
+		driver.findElement(By.id("input-password")).sendKeys(prop.getProperty("validPassword"));
+		driver.findElement(By.id("input-confirm")).sendKeys(prop.getProperty("validPassword"));
 		driver.findElement(By.xpath("//input[@name='newsletter'][@value='0']")).click();
 		driver.findElement(By.name("agree")).click();
 		driver.findElement(By.xpath("//input[@value='Continue']")).click();
