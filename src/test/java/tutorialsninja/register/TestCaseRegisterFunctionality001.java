59f8a922b306a0cd201563c3e009fb1cdbe6f56b
Mohammed Shahnawaz <shah4corp@gmail.com>
2025-07-19 22:30:39 +0530

    Framework update

--- src/test/java/tutorialsninja/register/TestCaseRegisterFunctionality001.java
+++ src/test/java/tutorialsninja/register/TestCaseRegisterFunctionality001.java
@@ -24,7 +24,6 @@ public class TestCaseRegisterFunctionality001 extends Base {
 		prop = CommonUtils.loadProperties();
 		LandingPage landingPage = new LandingPage(driver);
 		landingPage.clickOnMyAccount();
-		
 		landingPage.selectRegisterOption();
 	}
 
